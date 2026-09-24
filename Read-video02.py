84ace36a54f7dd7403714e0916382245dd0b7180
Bianca Hashimoto <bianca.hashimoto319@gmail.com>
2026-09-24 11:05:59 -0300

    Captura quadro de vídeo e salvamento

--- Read-video02.py
+++ Read-video02.py
@@ -1,6 +1,6 @@
 import cv2
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture("AnthonyShkraba.mp4")
 
 if vid.isOpened() == False:
     print("Não foi possível ler o feed da câmera")
@@ -14,10 +14,15 @@ print(width)
 fps = int(vid.get(cv2.CAP_PROP_FPS))
 print(fps)
 
+out = cv2.VideoWriter("Boxing.mp4", cv2.VideoWriter_fourcc(*"DIVX"),30 ,(width, height))
+
 while True:
     ret,frame = vid.read()
     cv2.imshow("webcam",frame)
-    if cv2.waitKey(1) == 32:
+    out.write(frame)
+    if cv2.waitKey(25) == 32:
         break
 
 vid.release()
+out.release()
+cv2.destroyAllWindows()
